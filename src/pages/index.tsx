ce21bde48340f6df0a5c64f1b435a546940d536c
Sadman Yasar Sayem <sadmanyasarsayem@gmail.com>
2023-10-02 01:01:30 +0800

    update animation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,77 +24,102 @@ export default function Home() {
     }
   }
 
-  async function predictionFunction() {
-    if (!model || !webcamRef.current?.video) return;
-
-    const predictions = await model.detect(webcamRef.current.video);
-    const cnvs = document.getElementById("myCanvas") as HTMLCanvasElement;
-    cnvs.width = webcamRef.current.video.videoWidth;
-    cnvs.height = webcamRef.current.video.videoHeight;
-
-    const ctx = cnvs.getContext("2d");
-    if (!ctx) return; // fix for 'ctx' is possibly 'null'
-    ctx.clearRect(
-      0,
-      0,
-      webcamRef.current.video.videoWidth,
-      webcamRef.current.video.videoHeight,
-    );
-
-    if (predictions.length > 0) {
-      console.log(predictions);
-      for (let n = 0; n < predictions.length; n++) {
-        // Check scores
-        console.log(n);
-        if (predictions[n]!.score > 0.8) {
-          const bboxLeft = predictions[n]!.bbox[0]; // fix for 'bboxLeft' is never reassigned
-          const bboxTop = predictions[n]!.bbox[1]; // fix for 'bboxTop' is never reassigned
-          const bboxWidth = predictions[n]!.bbox[2]; // fix for 'bboxWidth' is never reassigned
-          const bboxHeight = predictions[n]!.bbox[3]; // fix for 'bboxHeight' is never reassigned
-
-          console.log("bboxLeft: " + bboxLeft);
-          console.log("bboxTop: " + bboxTop);
-
-          console.log("bboxWidth: " + bboxWidth);
-
-          console.log("bboxHeight: " + bboxHeight);
-
-          ctx.beginPath();
-          ctx.font = "28px Arial"; // fix for 'ctx' is possibly 'null'
-          ctx.fillStyle = "red";
-
-          ctx.fillText(predictions[n]!.class, bboxLeft, bboxTop);
-
-          ctx.rect(bboxLeft, bboxTop, bboxWidth, bboxHeight);
-          ctx.strokeStyle = "#FF0000";
-
-          ctx.lineWidth = 5;
-          ctx.stroke();
-
-          console.log("detected");
-        }
-      }
-    }
-
-    setTimeout(() => {
-      // Your asynchronous code here
-      predictionFunction()
-        .then(() => {
-          // Code to run after the async operation completes
-        })
-        .catch((error) => {
-          // Handle any errors that occur during the async operation
-        });
-    }, 500);
-  }
-
   useEffect(() => {
     void (async () => {
       await tf.ready();
+      console.log("tf ready");
       await loadModel();
-      await predictionFunction();
+      console.log("model loaded");
     })();
   }, []);
+
+  useEffect(() => {
+    let req: number | null = null;
+
+    void (async () => {
+      async function predictionFunction() {
+        req = requestAnimationFrame(() => {
+          void (async () => {
+            await predictionFunction();
+          })();
+        });
+        console.log("predictionFunction");
+        if (!model || !webcamRef.current?.video) {
+          console.log(model, webcamRef);
+          return;
+        }
+
+        const predictions = await model.detect(webcamRef.current.video);
+        const cnvs = document.getElementById("myCanvas") as HTMLCanvasElement;
+        cnvs.width = webcamRef.current.video.videoWidth;
+        cnvs.height = webcamRef.current.video.videoHeight;
+
+        const ctx = cnvs.getContext("2d");
+        if (!ctx) return; // fix for 'ctx' is possibly 'null'
+        ctx.clearRect(
+          0,
+          0,
+          webcamRef.current.video.videoWidth,
+          webcamRef.current.video.videoHeight,
+        );
+
+        if (predictions.length > 0) {
+          console.log(predictions);
+          for (let n = 0; n < predictions.length; n++) {
+            // Check scores
+            console.log(n);
+            if (predictions[n]!.score > 0.8) {
+              const bboxLeft = predictions[n]!.bbox[0]; // fix for 'bboxLeft' is never reassigned
+              const bboxTop = predictions[n]!.bbox[1]; // fix for 'bboxTop' is never reassigned
+              const bboxWidth = predictions[n]!.bbox[2]; // fix for 'bboxWidth' is never reassigned
+              const bboxHeight = predictions[n]!.bbox[3]; // fix for 'bboxHeight' is never reassigned
+
+              console.log("bboxLeft: " + bboxLeft);
+              console.log("bboxTop: " + bboxTop);
+
+              console.log("bboxWidth: " + bboxWidth);
+
+              console.log("bboxHeight: " + bboxHeight);
+
+              ctx.beginPath();
+              ctx.font = "28px Arial"; // fix for 'ctx' is possibly 'null'
+              ctx.fillStyle = "red";
+
+              ctx.fillText(predictions[n]!.class, bboxLeft, bboxTop);
+
+              ctx.rect(bboxLeft, bboxTop, bboxWidth, bboxHeight);
+              ctx.strokeStyle = "#FF0000";
+
+              ctx.lineWidth = 5;
+              ctx.stroke();
+
+              console.log("detected");
+            }
+          }
+        }
+
+        // setTimeout(() => {
+        //   // Your asynchronous code here
+        //   predictionFunction()
+        //     .then(() => {
+        //       // Code to run after the async operation completes
+        //       console.log("predictionFunction THEN");
+        //     })
+        //     .catch((error) => {
+        //       // Handle any errors that occur during the async operation
+        //       console.log(error);
+        //     });
+        // }, 500);
+      }
+      await predictionFunction();
+    })();
+
+    return () => {
+      if (req) {
+        cancelAnimationFrame(req);
+      }
+    };
+  }, [model]);
 
   return (
     <>
@@ -108,10 +133,13 @@ export default function Home() {
           <div className="container absolute top-0">
             <Webcam id="img" ref={webcamRef} />
           </div>
-          <div className="absolute top-0 z-[9999] text-white opacity-50">
-            <canvas id="myCanvas" className="w-full" />
-          </div>
+          {model && (
+            <div className="absolute top-0 z-[9999] text-white opacity-50">
+              <canvas id="myCanvas" className="w-full" />
+            </div>
+          )}
         </>
+        {!model && <div className="text-2xl text-white">loading model</div>}
       </main>
     </>
   );
